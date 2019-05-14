0eac3261150d7c3451d4d672f48978b47a98cd2b
Andrea Fiandro <andrea.fiandro@gmail.com>
2019-05-14 10:32:21 +0200

    Inserita gestione autenticazione con JWT

--- src/main/java/it/polito/appinternet/pedibus/config/SecurityConfig.java
+++ src/main/java/it/polito/appinternet/pedibus/config/SecurityConfig.java
@@ -1,6 +1,11 @@
 package it.polito.appinternet.pedibus.config;
 
+import it.polito.appinternet.pedibus.security.JwtConfigurer;
+import it.polito.appinternet.pedibus.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,11 +14,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
+            .antMatchers("/login").permitAll()
             .antMatchers("/register").permitAll()
-            .antMatchers("/confirm").permitAll();
+            .antMatchers("/confirm").permitAll()
+        .and()
+        .apply(new JwtConfigurer(jwtTokenProvider));
     }
 }
---
+++ src/main/java/it/polito/appinternet/pedibus/security/InvalidJwtAuthenticationException.java
@@ -0,0 +1,9 @@
+package it.polito.appinternet.pedibus.security;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class InvalidJwtAuthenticationException extends AuthenticationException {
+    public InvalidJwtAuthenticationException(String e) {
+        super(e);
+    }
+}
