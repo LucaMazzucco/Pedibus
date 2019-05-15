ba756660ef9d718a8b5140557bdbf571a2603b2a
Andrea Fiandro <andrea.fiandro@gmail.com>
2019-05-15 14:59:32 +0200

    Non so cosa ho modificato ma ora la security funziona bene: - Da testare le richieste tipo /line con header JWT per vedere se è autorizzato

--- src/main/java/it/polito/appinternet/pedibus/config/SecurityConfig.java
+++ src/main/java/it/polito/appinternet/pedibus/config/SecurityConfig.java
@@ -1,18 +1,19 @@
 package it.polito.appinternet.pedibus.config;
 
-import it.polito.appinternet.pedibus.security.JwtConfigurer;
-import it.polito.appinternet.pedibus.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import it.polito.appinternet.pedibus.security.JwtTokenProvider;
+import it.polito.appinternet.pedibus.security.JwtConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 
     @Autowired
     JwtTokenProvider jwtTokenProvider;
@@ -25,12 +26,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/login").permitAll()
-            .antMatchers("/register").permitAll()
-            .antMatchers("/confirm").permitAll()
-        .and()
-        .apply(new JwtConfigurer(jwtTokenProvider));
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers( "/register").permitAll()
+                .antMatchers("/confirm/**").permitAll()
+                .antMatchers("/recover/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
+        //@formatter:on
     }
+
+
 }
