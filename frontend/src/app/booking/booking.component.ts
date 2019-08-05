3809d2a0f46fb9ccbee61ee15913a79b4d99a066
Andrea Fiandro <andrea.fiandro@gmail.com>
2019-08-05 12:19:17 +0200

    Inserita la possibilità di editare le cards con 2 action button

--- frontend/src/app/booking/booking.component.ts
+++ frontend/src/app/booking/booking.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {DataService} from "../services/data.service";
 import {Reservation} from "../classes/reservation";
 import {TitleService} from "../services/title.service";
-import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-booking',
@@ -15,15 +14,35 @@ export class BookingComponent implements OnInit {
 
   reservations: Reservation[];
   subscription: any;
+  isDisabled: Boolean[];
 
   ngOnInit() {
     this.getReservations();
     this.titleservice.changeTitle('Le mie prenotazioni')
+    this.isDisabled = []
   }
-
 
   getReservations() : void{
-    this.subscription = this.dataService.getReservations().subscribe(reservations => this.reservations = reservations)
+    this.subscription = this.dataService.getReservations().subscribe(reservations => {
+      this.reservations = reservations;
+      this.initializeArray();
+    })
   }
+
+  initializeArray(){
+    for(let i = 0; i < this.reservations.length; i++){
+      this.isDisabled.push(true);
+    }
+  }
+  enableEditing(index): void{
+    console.log(index);
+    if(this.isDisabled[index]){
+      this.isDisabled[index] = false;
+    }
+    else {
+      this.isDisabled[index] = true;
+    }
+  }
+
 
 }
