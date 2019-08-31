eea70a6fdd11c76538c210f8a11e9b5aac5b428c
albertobenincasa <benincasa.alberto@gmail.com>
2019-08-31 16:53:44 +0200

    frontend dovrebbe funzionare, bisogna aggiustare le date e vedere se effettivamente è cosi
    da fare? mettere checkbox per aggiungere più turni (anche lato backend
    in quel caso)

--- frontend/src/app/shift/shift.component.ts
+++ frontend/src/app/shift/shift.component.ts
@@ -12,16 +12,36 @@ import {MatDialog, MatDialogConfig, MatSnackBar, MatTableDataSource} from "@angu
 export class ShiftComponent implements OnInit {
 
   tableDataSource: MatTableDataSource<Shift> = new MatTableDataSource<Shift>([]);
-  displayedColumns: string[] = ['lineName', 'rideDate', 'flagGoing', 'delete'];
+  displayedColumns: string[] = ['lineName', 'rideDate', 'personName', 'flagGoing', 'delete', 'confirm'];
   infoMessage: string = '';
 
-  constructor(private dataService: DataService, private titleservice: TitleService) { }
+  constructor(private dataService: DataService, private _snackBar: MatSnackBar, private titleservice: TitleService) { }
 
   ngOnInit() {
     this.titleservice.changeTitle('Gestione turni accompagnatori');
     this.dataService.getShifts(localStorage.getItem('current_user')).subscribe(a => {
       this.tableDataSource.data = a;
     });
+  }
+
+  addShift(i: number){
+    //this.tableDataSource.data[i].confirmed = true
+    let response = this.dataService.addShift(this.tableDataSource.data[i])
+    //this.tableDataSource.data.splice(i,1);
+    //this.tableDataSource._updateChangeSubscription()
+    response.subscribe(data => {
+      this.infoMessage = "Aggiunto un nuovo turno!";
+      this._snackBar.open(this.infoMessage, '', {duration: 2000});
+      this.infoMessage = ''
+    }, error =>{
+      this.infoMessage = "Non è stato possibile aggiungere il turno";
+      this._snackBar.open(this.infoMessage, '', {duration: 2000});
+      this.infoMessage = '';
+    })
+  }
+
+  addShiftDebug(i: number){
+    console.log(this.tableDataSource.data[i])
   }
 
 }
