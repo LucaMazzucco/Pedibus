19b94235881b88be5198ddd20615d3e3ae53b1f2
Jean717p <jeanpi.francois@gmail.com>
2019-09-10 15:48:59 +0200

    Login json contains a string named "roles" con tutti i ruoli e frontend se lo salva in local storage con chiave current_user_roles. Per controllare i ruoli fare un .contains stile stringa dandogli in pasto il ruolo da controllare. i ruoli sono memorizzati lato frontend in una stringa tipo "[ROLE_ADMIN, ROLE_USER]"

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { tap, map, first } from 'rxjs/operators';
-import * as moment from "moment";
+import * as moment from 'moment';
 import { Observable } from 'rxjs';
 import * as jwt_decode from 'jwt-decode';
 
@@ -25,47 +25,53 @@ interface AdminRegistration {
   role: string;
 }
 
-export const TOKEN_NAME: string = 'jwt_token';
+export const TOKEN_NAME = 'jwt_token';
 @Injectable({
   providedIn: 'root',
 })
 
 
 export class AuthService {
-  
+
   constructor(private http: HttpClient) { }
 
-  login(username:string, password:string ) {
+  login(username: string, password: string ) {
     return this.http.post<UserLogin>(REST_URL + '/login', {username, password});
   }
-  
+
   setSession(authResult) {
-    if (this.getToken() != null){
-      this.logout()
+    if (this.getToken() != null) {
+      this.logout();
     }
     localStorage.setItem('current_user', authResult.email);
     localStorage.setItem(TOKEN_NAME, authResult.token);
+    localStorage.setItem('current_user_roles', authResult.roles);
   }
   logout() {
     localStorage.removeItem('current_user');
     localStorage.removeItem(TOKEN_NAME);
+    localStorage.removeItem('current_user_roles');
   }
 
   getToken(): string {
     return localStorage.getItem(TOKEN_NAME);
   }
 
+  getRoles(): string {
+    return localStorage.getItem('current_user_roles');
+  }
+
   isLoggedIn(token?: string): boolean {
-    if(!token) token = this.getToken();
-    if(!token) return false;
+    if (!token) { token = this.getToken(); }
+    if (!token) { return false; }
     const date = this.getTokenExpirationDate(token);
-    if(date === undefined) return false;
+    if (date === undefined) { return false; }
     return (date.valueOf() > new Date().valueOf());
   }
 
   getTokenExpirationDate(token: string): Date {
     const decoded = jwt_decode(token);
-    if (decoded.exp === undefined) return null;
+    if (decoded.exp === undefined) { return null; }
 
     const date = new Date(0);
     date.setUTCSeconds(decoded.exp);
@@ -76,19 +82,21 @@ export class AuthService {
       return !this.isLoggedIn();
   }
 
-  getEmailPresence(email: string): Observable<Boolean>{
-    return this.http.get<Boolean>(REST_URL + "/checkEmail/" + email);                            
-  }
-  
-  register(name: String, surname: String, registrationNumber: String, email: String, password: String, token: String): Observable<HttpResponse<UserRegistration>>{
-    return this.http.post<UserRegistration>(REST_URL + '/register/' + token, {name, surname, registrationNumber, email, password}, {observe: 'response'})
+  getEmailPresence(email: string): Observable<boolean> {
+    return this.http.get<boolean>(REST_URL + '/checkEmail/' + email);
   }
 
-  registerAdmin(email: String, role: String, line: String): Observable<HttpResponse<AdminRegistration>>{
+  // tslint:disable-next-line:max-line-length
+  register(name: string, surname: string, registrationNumber: string, email: string, password: string, token: string): Observable<HttpResponse<UserRegistration>> {
+    // tslint:disable-next-line:max-line-length
+    return this.http.post<UserRegistration>(REST_URL + '/register/' + token, {name, surname, registrationNumber, email, password}, {observe: 'response'});
+  }
+
+  registerAdmin(email: string, role: string, line: string): Observable<HttpResponse<AdminRegistration>> {
     return this.http.post<AdminRegistration>(REST_URL + '/registerAdmin', {email, role, line}, {observe: 'response'});
   }
 
-  checkToken(email: String, token: String): Observable<Boolean>{
-    return this.http.get<Boolean>(REST_URL + '/checkToken/' + email + '/' + token);
+  checkToken(email: string, token: string): Observable<boolean> {
+    return this.http.get<boolean>(REST_URL + '/checkToken/' + email + '/' + token);
   }
 }
