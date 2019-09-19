ec3fac2f52562e8a3821cebd6cf58be7c2b7d3b7
Andrea Fiandro <andrea.fiandro@gmail.com>
2019-09-19 11:40:57 +0200

    Aggiungi bambino sistemato

--- frontend/src/app/model/child.ts
+++ frontend/src/app/model/child.ts
@@ -10,11 +10,12 @@ export class Child {
 
 
 
-    constructor(name: string, surname: string, registrationNumber: string, defaultLine: string, defaultStop: string) {
+    constructor(name: string, surname: string, registrationNumber: string, defaultLine: string, defaultStop: string, availableStops: string[]) {
         this.name = name;
         this.surname = surname;
         this.registrationNumber = registrationNumber;
         this.defaultLine = defaultLine;
         this.defaultStop = defaultStop;
+        this.availableStops = availableStops;
     }
 }
